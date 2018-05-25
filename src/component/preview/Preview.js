ec00b36134b6d30f081bf2b8d5367384e3430e4c
Isabella Li <yiman.isabella@gmail.com>
2018-05-25 16:48:20 +1000

    WIP, add preview page, about to eject configs

--- src/component/preview/Preview.js
+++ src/component/preview/Preview.js
@@ -1,24 +1,60 @@
 import React from 'react';
 import ejs from 'ejs';
 import fs from 'fs';
+import { connect } from 'react-redux';
 
-import logger from '../../utils/logger';
+import { fetchEvents, errorOccurred } from '../../redux/meetup';
+import '../App.css';
 
-const Preview = (groupedEvents) => {
+const Error = () => (
+  <div className="App">
+    <p className="App-intro">
+      An Error occurred.
+    </p>
+  </div>
+)
+
+const formatEvents = (groupedEvents, dispatchErrorOccurred) => {
   try {
     const data = fs.readFileSync(`${__dirname}/emailTemplate.ejs`, 'utf8');
     const template = ejs.compile(data);
     return template({ groupedEvents });
   } catch (err) {
-    logger.error('unable to template events, with err: ', err);
-    return (
-      <div className="App">
-        <p className="App-intro">
-          An Error occurred.
-        </p>
-      </div>
-    )
+    console.log(err);
+    dispatchErrorOccurred();
   }
 }
 
-export default Preview;
+const Preview = ({
+  groupedEvents,
+  isLoading,
+  hasError,
+  dispatchFetchEvents,
+  dispatchErrorOccurred,
+}) => {
+  const loader = isLoading ? <div className="loader"/>: null;
+  const error = hasError ? <Error /> : null;
+  const events = (!isLoading && !hasError && groupedEvents) ? formatEvents(groupedEvents, dispatchErrorOccurred) : null;
+
+  return (
+    <div>
+      <button onClick={() => dispatchFetchEvents('28/05/2018')}>Fetch Event</button>
+      {loader}
+      {error}
+      {console.log(events)}
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  isLoading: state.meetup.isLoading,
+  hasError: state.meetup.hasError,
+  groupedEvents: state.meetup.groupedEvents,
+})
+
+const mapDispatchToProps = dispatch => ({
+  dispatchFetchEvents: date => dispatch(fetchEvents(date)),
+  dispatchErrorOccurred: () => dispatch(errorOccurred()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Preview);
--- src/redux/meetup.js
+++ src/redux/meetup.js
@@ -1,7 +1,58 @@
-const initialState = {};
+import axios from 'axios';
+
+const FETCH_EVENT_LOADING = 'FETCH_EVENT_LOADING';
+const FETCH_EVENT_ERROR = 'FETCH_EVENT_ERROR';
+const FETCH_EVENT_SUCCESS = 'FETCH_EVENT_SUCCESS';
+
+export const fetchEvents = date => dispatch => {
+  dispatch({ type: FETCH_EVENT_LOADING });
+  axios.post('https://generator-meetup-tw.herokuapp.com/generate', {
+    date,
+  })
+    .then((response) => {
+      dispatch({
+        type: FETCH_EVENT_SUCCESS,
+        groupedEvents: response.data,
+      })
+    })
+    .catch(() => {
+      dispatch(errorOccurred());
+    });
+}
+
+export const errorOccurred = () => ({
+  type: FETCH_EVENT_ERROR
+})
+
+const initialState = {
+  isLoading: false,
+  hasError: false,
+  groupedEvents: undefined,
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_EVENT_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+        hasError: false,
+        groupedEvents: undefined,
+      }
+    case FETCH_EVENT_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        hasError: true,
+        groupedEvents: undefined,
+      }
+    case FETCH_EVENT_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        hasError: false,
+        groupedEvents: action.groupedEvents,
+      }
     default:
       return state;
   }
