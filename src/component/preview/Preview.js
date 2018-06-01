5e57caec2c88ad58da8e512cb6405c9d096ebe34
Isabella Li <yiman.isabella@gmail.com>
2018-06-01 12:07:31 +1000

    style the preview page

--- src/component/preview/Preview.js
+++ src/component/preview/Preview.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
-import { Button } from 'react-bootstrap';
+import { Button, Panel, Grid, Row, Col } from 'react-bootstrap';
 import 'react-datepicker/dist/react-datepicker.css';
 
 import { fetchEvents } from '../../redux/meetup';
@@ -12,6 +12,13 @@ import './Preview.css';
 const createMarkup = htmlContent => ({
   __html: htmlContent,
 });
+
+const bodyStyle = {
+  display: 'block',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  width: '50%',
+};
 
 class Preview extends Component {
   constructor(props) {
@@ -35,19 +42,37 @@ class Preview extends Component {
 
     return (
       <div>
-        <DatePicker
-          selected={this.state.startDate}
-          onChange={date => this.setState({ startDate: date })}
-          dateFormat="DD/MM/YYYY"
-        />
-        <Button
-          onClick={() => dispatchFetchEvents(this.state.startDate)}
-
-        >Preview Event</Button>
-        {loader}
-        {error}
-        {events}
-      </div>
+        <Panel>
+          <Panel.Body>
+            <Grid>
+              <Row className="show-grid">
+                <Col xs={6} md={4} style={{ textAlign: 'center' }}>
+                  <Panel.Title>Pick a Date</Panel.Title>
+                </Col>
+                <Col xs={6} md={4}>
+                  <DatePicker
+                    selected={this.state.startDate}
+                    onChange={date => this.setState({ startDate: date })}
+                    dateFormat="DD/MM/YYYY"
+                  />
+                </Col>
+                <Col xs={6} md={4}>
+                  <Button
+                    onClick={() => dispatchFetchEvents(this.state.startDate)}
+                  >
+                    Preview Event
+                  </Button>
+                </Col>
+              </Row>
+            </Grid>
+          </Panel.Body>
+        </Panel>
+        <div style={bodyStyle}>
+          {loader}
+          {error}
+          {events}
+        </div>
+      </div >
     );
   }
 }
