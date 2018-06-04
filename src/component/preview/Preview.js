3fb3b0b381c9dcb3471cc5bd51f677582ea48838
Isabella Li <yiman.isabella@gmail.com>
2018-06-04 16:17:07 +1000

    style preview page to keep consistent

--- src/component/preview/Preview.js
+++ src/component/preview/Preview.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
-import { Button, Panel, Grid, Row, Col } from 'react-bootstrap';
+import { Button, Well, Grid, Row, Col } from 'react-bootstrap';
 import 'react-datepicker/dist/react-datepicker.css';
 import FontAwesome from 'react-fontawesome';
 
@@ -18,7 +18,7 @@ const bodyStyle = {
   display: 'block',
   marginLeft: 'auto',
   marginRight: 'auto',
-  width: '50%',
+  width: '65%',
 };
 
 class Preview extends Component {
@@ -42,32 +42,30 @@ class Preview extends Component {
       <div dangerouslySetInnerHTML={createMarkup(eventsHTML)} /> : null;
 
     return (
-      <div>
-        <Panel>
-          <Panel.Body>
-            <Grid>
-              <Row className="show-grid">
-                <Col xs={6} md={4} style={{ textAlign: 'center' }}>
-                  <Panel.Title>Pick a Date</Panel.Title>
-                </Col>
-                <Col xs={6} md={4}>
-                  <DatePicker
-                    selected={this.state.startDate}
-                    onChange={date => this.setState({ startDate: date })}
-                    dateFormat="DD/MM/YYYY"
-                  />
-                </Col>
-                <Col xs={6} md={4}>
-                  <Button
-                    onClick={() => dispatchFetchEvents(this.state.startDate)}
-                  >
+      <div style={{ padding: '0 10%' }}>
+        <Well>
+          <Grid>
+            <Row className="show-grid">
+              <Col xs={6} md={4} style={{ textAlign: 'center' }}>
+                <h5>Pick a Date:</h5>
+              </Col>
+              <Col xs={6} md={4}>
+                <DatePicker
+                  selected={this.state.startDate}
+                  onChange={date => this.setState({ startDate: date })}
+                  dateFormat="DD/MM/YYYY"
+                />
+              </Col>
+              <Col xs={6} md={4}>
+                <Button
+                  onClick={() => dispatchFetchEvents(this.state.startDate)}
+                >
                     Preview Event
-                  </Button>
-                </Col>
-              </Row>
-            </Grid>
-          </Panel.Body>
-        </Panel>
+                </Button>
+              </Col>
+            </Row>
+          </Grid>
+        </Well>
         <div style={bodyStyle}>
           {loader}
           {error}
